f10e888a020816a0c8d5ebd3e24e8ce77cdca10a
anubhav17T <anubhav1tyagi@gmail.com>
2023-03-14 00:08:53 +0530

    url modified

--- src/utils/connections/db_object.py
+++ src/utils/connections/db_object.py
@@ -5,7 +5,7 @@ from src.utils.custom_exceptions.custom_exceptions import DatabaseConnectionErro
 from src.utils.logger.logger import logger
 try:
     logger.info(" ########## GOING FOR CONNECTION ##############")
-    db = Database(DB_URL)
+    db = Database("postgresql://postgres:Anubhav1234@roomshalainstancetest.clwy0rgkmro3.us-east-1.rds.amazonaws.com:5432/roomshala_admin")
 except Exception as e:
     logger.error("###### EXCEPTION IN DB_OBJECT IS {} ###########".format(e))
     raise DatabaseConnectionError(message="Error Occur in Database {}".format(e))
